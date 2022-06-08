733b88f69d9c96a5d92857e47cf8cba40d86cb84
SaiKiran <saikiranchatlapalli84@gmail.com>
2022-06-09 00:34:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Forgot from "./pages/Forgot";
+import Reset from "./pages/Reset";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar"; 
 
-function App() {
+const App = () => {
+ 
+  const token = useSelector((state) => state.token.value);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+
+        {/* Authenticated  Routes */}
+        <Route index element={token ? <Home/> : <Login />}/>
+
+        {/* Unauthenticated Routes */}
+        <Route path="/login" element={token ? <Navigate replace to="/" /> : <Login />}/>
+        <Route path="/register" element={token ? <Navigate replace to="/" /> : <Register />}/>
+        <Route path="/forgot" element={<Forgot/>} />
+        <Route path="/reset" element={<Reset />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
